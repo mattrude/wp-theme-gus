7b3d7fa9097138c7e29f705f61438d26acf8c182
Matt Rude <m@mattrude.com>
2012-05-29 03:44:39 +0000

    more updates

--- archive.php
+++ archive.php
@@ -3,10 +3,10 @@
 	    	<div id=page class=content>
 				<div id=home>
 					<?php if (have_posts()) : ?>
-						<h3 style=padding-top:0.75em>Recent Posts <a href="/archives" class=more>view all posts</a></h3>
+						<h3 style=padding-top:0.75em>Archive Posts</a></h3>
 						<ul class=posts>
+							<!--Starting "The Loop"-->
 							<?php while (have_posts()) : the_post(); ?>
-								<!--Starting "The Loop"-->
 								<li>
 									<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a>
 									<time datetime="<?php the_date('M d y'); ?>" pubdate="pubdate"><?php the_date('Y-m-d'); ?></time>
