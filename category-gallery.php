2dee61a63e69b371760c3ff951b67a7619fce470
Matt Rude <m@mattrude.com>
2012-05-29 03:57:02 +0000

    white space cleanup

--- category-gallery.php
+++ category-gallery.php
@@ -1,52 +1,52 @@
 <?php get_header(); ?>
-		<div id=main role=main>
-	    	<div id=page class=content>
-				<div id=home>
-					<?php if (have_posts()) : ?>
-						<h3 style=padding-top:0.75em>Recent Posts <a href="/archives" class=more>view all posts</a></h3>
-						<ul class=posts>
-							<!--Starting "The Loop"-->
-							<?php while (have_posts()) : the_post(); ?>
-								<div id="gallerypost-<?php the_ID(); ?>" class="gallerypost post">
-									<div id="gallerypost_main-<?php the_ID(); ?>" class="gallerypost_main">
-										<div id="gallerypost_thumbnail-<?php the_ID(); ?>" class="gallerypost_thumbnail">
-											<a href="<?php the_permalink() ?>"><?php the_post_thumbnail() ?></a>
-										</div>
-										<div id="gallerypost_body-<?php the_ID(); ?>" class="gallerypost_body">
-											<?php $images =& get_children( 'post_type=attachment&post_mime_type=image' ); ?>
-											<h1 class="single-title entry-title">
-												<a rel="bookmark" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-											</h1>
-											<div class="byline">
-												<span class="byline-prep byline-prep-author text">Album created by </span>
-												<span class="author"><?php the_author(); ?></span>
-												<span class="byline-prep byline-prep-author text"> on </span>
-												<span class="published updated"><?php the_time('F jS, Y') ?></span>
-												<span class="byline-prep byline-prep-author text"> <?php edit_post_link('Edit', ' | '); ?> </span>
-											</div>
-											<div class="entry">
-												<?php the_excerpt(); ?>
-											</div>
-										</div>
+<div id=main role=main>
+   	<div id=page class=content>
+		<div id=home>
+			<?php if (have_posts()) : ?>
+				<h3 style=padding-top:0.75em>Recent Posts <a href="/archives" class=more>view all posts</a></h3>
+				<ul class=posts>
+					<!--Starting "The Loop"-->
+					<?php while (have_posts()) : the_post(); ?>
+						<div id="gallerypost-<?php the_ID(); ?>" class="gallerypost post">
+							<div id="gallerypost_main-<?php the_ID(); ?>" class="gallerypost_main">
+								<div id="gallerypost_thumbnail-<?php the_ID(); ?>" class="gallerypost_thumbnail">
+									<a href="<?php the_permalink() ?>"><?php the_post_thumbnail() ?></a>
+								</div>
+								<div id="gallerypost_body-<?php the_ID(); ?>" class="gallerypost_body">
+									<?php $images =& get_children( 'post_type=attachment&post_mime_type=image' ); ?>
+									<h1 class="single-title entry-title">
+										<a rel="bookmark" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+									</h1>
+									<div class="byline">
+										<span class="byline-prep byline-prep-author text">Album created by </span>
+										<span class="author"><?php the_author(); ?></span>
+										<span class="byline-prep byline-prep-author text"> on </span>
+										<span class="published updated"><?php the_time('F jS, Y') ?></span>
+										<span class="byline-prep byline-prep-author text"> <?php edit_post_link('Edit', ' | '); ?> </span>
 									</div>
-									<div id="gallerypost_sub-<?php the_ID(); ?>" class="gallerypost_sub">
-										<div id="gallerypost_sub_left-<?php the_ID(); ?>" class="gallerypost_sub_left">
-											<p><?php echo get_the_term_list( $post->ID, 'people', 'Who: ', ', ', '<br />' ); ?></p>
-											<p><?php echo get_the_term_list( $post->ID, 'events', 'What: ', ', ', '<br />' ); ?></p>
-											<p><?php echo get_the_term_list( $post->ID, 'places', 'Where: ', ', ', '' ); ?></p>
-										</div>
-										<?php $num_gallery_posts = $wpdb->get_var( "SELECT COUNT(*) FROM $wpdb->posts WHERE post_parent = '$post->ID' AND post_type = 'attachment'" );
-        								if ( $num_gallery_posts > 1 ) { ?>
-											<div id="gallerypost_sub_right-<?php the_ID(); ?>" class="gallerypost_sub_right">
-												<small>This Album contains <?php echo $num_gallery_posts; ?> items.</small>
-											</div>
-										<?php } ?>
+									<div class="entry">
+										<?php the_excerpt(); ?>
 									</div>
 								</div>
-							<?php endwhile; ?>
-						</ul>
-					<?php endif; ?>
-				</div>
-			</div>
+							</div>
+							<div id="gallerypost_sub-<?php the_ID(); ?>" class="gallerypost_sub">
+								<div id="gallerypost_sub_left-<?php the_ID(); ?>" class="gallerypost_sub_left">
+									<p><?php echo get_the_term_list( $post->ID, 'people', 'Who: ', ', ', '<br />' ); ?></p>
+									<p><?php echo get_the_term_list( $post->ID, 'events', 'What: ', ', ', '<br />' ); ?></p>
+									<p><?php echo get_the_term_list( $post->ID, 'places', 'Where: ', ', ', '' ); ?></p>
+								</div>
+								<?php $num_gallery_posts = $wpdb->get_var( "SELECT COUNT(*) FROM $wpdb->posts WHERE post_parent = '$post->ID' AND post_type = 'attachment'" );
+								if ( $num_gallery_posts > 1 ) { ?>
+									<div id="gallerypost_sub_right-<?php the_ID(); ?>" class="gallerypost_sub_right">
+										<small>This Album contains <?php echo $num_gallery_posts; ?> items.</small>
+									</div>
+								<?php } ?>
+							</div>
+						</div>
+					<?php endwhile; ?>
+				</ul>
+			<?php endif; ?>
 		</div>
+	</div>
+</div>
 <?php get_footer(); ?>
