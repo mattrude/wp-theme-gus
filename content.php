964220c8a60f27c53f9342c4b72f9985dc83c122
Matt Rude <matt@mattrude.com>
2012-12-31 17:11:34 -0600

    Add tool tips to Previous and Next links
    Fixes #25

--- content.php
+++ content.php
@@ -20,8 +20,8 @@
 
 	<nav id="nav-single">
 		<h3 class="assistive-text"><?php _e( 'Post navigation', 'twentyeleven' ); ?></h3>
-		<span class="nav-previous"><?php previous_post_link( '%link', __( '<span class="meta-nav">&larr;</span> Previous', 'twentyeleven' ) ); ?></span>
-		<span class="nav-next"><?php next_post_link( '%link', __( 'Next <span class="meta-nav">&rarr;</span>', 'twentyeleven' ) ); ?></span>
+        <span class="nav-previous"><?php previous_post_link_plus( array( 'order_by' => 'post_date', 'format' => '%link', 'link' => __( '<span class="meta-nav">&larr;</span> Previous', 'twentyeleven' ), 'tooltip' => '%title' ) ); ?></span>
+        <span class="nav-next"><?php next_post_link_plus( array( 'order_by' => 'post_date', 'format' => '%link', 'link' => __( 'Next <span class="meta-nav">&rarr;</span>', 'twentyeleven' ) ) ); ?></span>
 	</nav><!-- #nav-single -->
 
 
--- single.php
+++ single.php
@@ -3,7 +3,6 @@
 		<div id=post class=content itemscope itemtype="http://schema.org/Article">
 			<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 				<div <?php post_class(); ?> id="post-<?php the_ID(); ?>">
-					<?php gus_content_nav('nav-top-right'); ?>
 					<?php get_template_part( 'content', get_post_format() ); ?>
 					&nbsp;
 				</div><!-- Ending ID post-<?php the_ID(); ?> -->
